ebabb73bd02025b64d93d82892bb334818264664
KatanMichael <32782995+KatanMichael@users.noreply.github.com>
2017-10-26 15:46:51 +0300

    Reset

--- src/basic_java/Main.java
+++ src/basic_java/Main.java
@@ -6,27 +6,7 @@ public class Main
 {
     public static void main(String args[])
     {
-        int x = 0, y = 0, z = 0;
 
-        Scanner scan = new Scanner(System.in);
-
-        Integer b = new Integer(10);
-        Integer a = new Integer(20);
-
-        System.out.println("A is: "+a.intValue() + " And B is:" +b.intValue());
-
-        swap(a,b);
-
-        System.out.println("A is: "+a.intValue() + " And B is:" +b.intValue());
-
-    }
-
-    public static void swap(Integer a, Integer b)
-    {
-        Integer temp;
-        temp = a;
-        a = b;
-        b = temp;
 
     }
 }
