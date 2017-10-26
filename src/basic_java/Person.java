87487250eddb284af4ee2201754d5873dfd2c1cc
KatanMichael <32782995+KatanMichael@users.noreply.github.com>
2017-10-26 14:22:34 +0300

    Adding Person Class

---
+++ src/basic_java/Person.java
@@ -0,0 +1,33 @@
+package basic_java;
+
+public class Person
+{
+    int age;
+    int id;
+    String name;
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
