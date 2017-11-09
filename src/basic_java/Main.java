6fff268951be8f693e7cc35a1606246af20c5b1c
katanmichael <michaelkatan@gmail.com>
2017-11-09 12:45:04 +0200

    First Commit

--- src/basic_java/Main.java
+++ src/basic_java/Main.java
@@ -4,24 +4,6 @@ public class Main
 {
     public static void main(String args[])
     {
-        Student m = new Student(6, 3049, 27, "Michael");
-        Person s = new Person(27, 2011, "Sapir");
-
-        Person e = new Student(6, 1234, 24, "Eddie");
-
-        System.out.println("OMG!!!!");
-        System.out.println("Can you see that?");
-
-        m.giveWallet(new Wallet());
-
-        m.chackWallet().setCapacity(100);
-        m.chackWallet().fillMoney(20.5);
-
-        System.out.println(m);
-        System.out.println(s);
-
-
-
 
 
     }
