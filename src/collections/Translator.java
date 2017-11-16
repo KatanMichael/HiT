0c27a75203f94112f07a96bc9f1c9c89a7f6f585
katanmichael <michaelkatan@gmail.com>
2017-11-16 20:04:10 +0200

    Translator Class Completed...

--- src/collections/Translator.java
+++ src/collections/Translator.java
@@ -1,11 +1,14 @@
 package collections;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Set;
 
 public class Translator {
     private HashMap<String, String> dicto;
 
-    public Translator(HashMap<String, String> dicto) {
+    public Translator() {
         this.dicto = new HashMap<>();
     }
 
@@ -13,9 +16,28 @@ public class Translator {
         dicto.put(hebWord, engWord);
     }
 
-    public void translate(String sentence) {
+    public String translate(String sentence) {
+        String[] temp;
+        temp = sentence.split(" ");
+        String tempTrans = "";
 
+        for (int i = 0; i < temp.length; i++) {
+            if (dicto.get(temp[i]) != null) {
+                tempTrans = tempTrans + (" " + dicto.get(temp[i]));
+            } else {
+                tempTrans = tempTrans + (" " + temp[i]);
+            }
+        }
+
+        return tempTrans;
     }
 
+    public ArrayList<String> getEngWords() {
+        Set<String> set = dicto.keySet();
+        Collection<String> col = dicto.keySet();
+        ArrayList<String> arr = new ArrayList<>();
+        arr.addAll(col);
+        return arr;
+    }
 
 }
